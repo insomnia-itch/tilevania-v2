4ecb8ee57ddfac170d6648ee65e550595424da31
Jelani Woods <jelani@firstdraft.com>
2022-12-17 22:24:09 -0600

    Flip sprite when moving

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,18 +6,32 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D rb;
+
+    [SerializeField] float runSpeed = 10f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run() {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+    }
+
+    void FlipSprite() {
+        bool moving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (moving)
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
     }
 }
