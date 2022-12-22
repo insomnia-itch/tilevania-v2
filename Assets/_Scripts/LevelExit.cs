c1904c49da55ed817ea145cca7a31ba2e20d1b74
Jelani Woods <jelani@firstdraft.com>
2022-12-21 19:11:02 -0600

    Level loading

---
+++ Assets/_Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float loadTimeDelay = 0.5f;
+    IEnumerator LoadNextLevel() {
+        yield return new WaitForSecondsRealtime(loadTimeDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        int totalSceneCount = SceneManager.sceneCountInBuildSettings;
+        if (nextSceneIndex == totalSceneCount)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        StartCoroutine(LoadNextLevel());
+    }
+}
